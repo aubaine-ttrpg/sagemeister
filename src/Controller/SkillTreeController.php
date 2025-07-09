7a87217a585d164316cab98de6f406d17dc7dd3f
a-y-men <aymen@layan.eu>
2025-07-09 16:40:15 +0200

    🚧 Render SkillTree

--- src/Controller/SkillTreeController.php
+++ src/Controller/SkillTreeController.php
@@ -18,33 +18,57 @@ class SkillTreeController extends AbstractController
         $treePage = $this->notion->retrievePage($id);
 
         $titleBlocks = $treePage['properties']['Name']['title'] ?? [];
-        $treeName = count($titleBlocks)
+        $skilltreeName = count($titleBlocks)
             ? $titleBlocks[0]['plain_text']
-            : $id;  // fallback to ID if no title set
+            : $id;
 
-        $filter = [
-            'property' => 'Arbre',
-            'relation' => ['contains' => $id],
-        ];
-        $sorts = [[
-            'property'  => 'Position',
-            'direction' => 'ascending',
-        ]];
-
-        $response   = $this->notion->queryDatabase(
+        $response = $this->notion->queryDatabase(
             NotionDatabaseId::CAPACITY->value,
             [
-                'filter'    => $filter,
-                'sorts'     => $sorts,
+                'filter'    => ['property' => 'Arbre', 'relation' => ['contains' => $id]],
+                'sorts'     => [['property' => 'Position', 'direction' => 'ascending']],
                 'page_size' => 100,
             ]
         );
-        $capacities = $response['results'] ?? [];
+        $rawCaps = $response['results'] ?? [];
+
+        $nameMap = [];
+        foreach ($rawCaps as $cap) {
+            $titles = $cap['properties']['Name']['title'] ?? [];
+            $nameMap[$cap['id']] = $titles
+                ? $titles[0]['plain_text']
+                : 'UNKNOWN CAPACITY';
+        }
+
+        $nodes = [];
+        foreach ($rawCaps as $cap) {
+            // parse Position: expect "row,column"
+            $pos = $cap['properties']['Position']['rich_text'][0]['plain_text'] ?? '';
+            if (!preg_match('/^\s*(\d+)\s*,\s*(\d+)\s*$/', $pos, $m)) {
+                // skip invalid or missing
+                continue;
+            }
+            [$_, $row, $col] = $m;
+
+            $rels = $cap['properties']['Prérequis (Capacité)']['relation'] ?? [];
+            $edges = [];
+            foreach ($rels as $r) {
+                if (isset($nameMap[$r['id']])) {
+                    $edges[] = $nameMap[$r['id']];
+                }
+            }
+
+            $nodes[] = [
+                'name'  => $nameMap[$cap['id']],
+                'x'     => (int) $col,
+                'y'     => (int) $row,
+                'edges' => $edges,
+            ];
+        }
 
         return $this->render('skilltree/index.html.twig', [
-            'pageId'     => $id,
-            'treeName'   => $treeName,
-            'capacities' => $capacities,
+            'skilltree_name' => $skilltreeName,
+            'nodes' => $nodes,
         ]);
     }
 }
